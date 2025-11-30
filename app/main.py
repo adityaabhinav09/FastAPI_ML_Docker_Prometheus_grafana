951681a49b91a5d357348a9eff83497e62148314
adityaabhinav09 <adityaabhinav108@gmail.com>
2025-11-30 10:33:10 +0530

    Added middleware and main.py

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.core.exception import register_exception_handlers
+
+app = FastAPI(title="Car Price Prediction API")
+
+# Linking middleware to app.
+
+app.add_middleware(LoggingMiddleware)
+
+# Linking endpoints to app
+
+app.include_router(routes_auth.router, tags=['Auth'])
+app.include_router(routes_predict.router, tags=['Prediction'])
+
+# Monitoring using Prometheus
+
+Instrumentator().instrument(app).expose(app)
+
+# Add exception handlers
+
+register_exception_handlers(app)
+
+
---
+++ app/middleware/logging_middleware.py
@@ -0,0 +1,12 @@
+from fastapi import middleware
+from pydantic import BaseModel
+import logging
+from starlette.middleware import BaseHTTPMiddleware
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self,request,call_next):
+        logging.info(f'Request:{request.method} {request.url}')
+        response = await call_next(request)
+        logging.info(f'Response: {response.status_code}')
+        return response
